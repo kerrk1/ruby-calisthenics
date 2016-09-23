7263dbb791aca9136f5813a6d2e011f993995bed
Kristin <kerrk1@citadel.edu>
2016-09-23 02:41:58 +0000

    palindrome and word_count complete

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,10 +1,20 @@
 module FunWithStrings
-  def palindrome?(string)
-    r_string = string.reverse
-    return true if r_string == string
+  def palindrome?
+    self.downcase!
+    self.gsub!(/\W/, '') 
+    reverse = self.reverse
+    return true if self == reverse
   end
   def count_words
-    # your code here
+    self.downcase!
+    words = self.split()
+    word_count = Hash.new(0)
+    words.each do |x|
+      x.gsub!(/\W+/, '')
+      word_count[x] += 1
+    end
+    word_count.delete('')
+    return word_count
   end
   def anagram_groups
     # your code here
